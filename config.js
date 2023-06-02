895efd0462b12ff26975d1b597500a14b7f79b46
gbaoooo <135301778+gbaoooo@users.noreply.github.com>
2023-06-02 12:39:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh =))',
-    btnNo: 'Không đâu bé à =)))',
+    btnYes: 'Đúng vậy em iu anhh',
+    btnNo: 'không phải,em vẫn giận anh',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại iu anhhh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Tại anh qá là dth',
+    reply: 'Tại anh qá là dth nên emm không thể giận anh nữaaaa',
     mess: 'Anh biết mà 🥰. Iu bée nhiều nhiều 😘😘',
     messDesc: 'Thế còn đợi gì mà không hết giận anh đi.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: '=))))))0',
+    messLink: 'https://photos.app.goo.gl/8QxL5fJcNnUknLTD7' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
