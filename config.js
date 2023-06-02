6cef8fb21978ded0ee788d4bb8e81ddc80d62217
gbaoooo <135301778+gbaoooo@users.noreply.github.com>
2023-06-02 11:51:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chị iu à!',
+    introTitle: 'Bé iu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em hehee <33',
-    btnNo: 'Không, bae à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích em <3',
-    btnReply: 'Gửi cho <3',
-    reply: 'Tại em quá là dễ thương hehe <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Thế còn đợi gì mà không ib cho em đii.',
+    btnYes: 'Vẫn cứ là thích anh =))',
+    btnNo: 'Không đâu bé à =)))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại iu anhhh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại anh qá là dth',
+    mess: 'Anh biết mà 🥰. Iu bée nhiều nhiều 😘😘',
+    messDesc: 'Thế còn đợi gì mà không hết giận anh đi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
